1b42151a248ed9f8ad794eb57dde01f665841656
satisperfectum <47039484+satisperfectum@users.noreply.github.com>
2020-01-17 00:42:39 +0300

    Reverse NAMES!

---
+++ ClientSide.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+namespace TCPClientApplication
+{
+    public partial class ClientSide : Form
+    {
+        //IP Address is local.
+        private string ipAddress;
+
+        public ClientSide()
+        {
+            InitializeComponent();
+        }
+        
+        int port = 13000;
+        Socket requestPortListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private void RequestPortButton_Click(object sender, EventArgs e)
+        {
+            ipAddress = textBox1.Text;
+            RequestForPort();
+        }
+
+        private void RequestForPort()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+            requestPortListener.Bind(ep);
+            requestPortListener.Listen(100);
+
+            Console.WriteLine("Client requested an available port.");
+            Socket serverPortReply = default(Socket);
+            serverPortReply = requestPortListener.Accept();
+
+            Thread clientThread = new Thread(new ThreadStart(() => ConnectedClient(serverPortReply, requestPortListener)));
+            clientThread.Start();
+        }
+        string assignedPort;
+        private delegate void setTextDel();
+
+        private void ConnectedClient(Socket serverPortReply, Socket requestPortListener)
+        {
+            byte[] messageFromServer = new byte[1024];
+            int size = serverPortReply.Receive(messageFromServer);
+            assignedPort = Encoding.ASCII.GetString(messageFromServer, 0, size);
+
+
+            DataToSendTextBox.Invoke(new setTextDel(SetText));
+            Console.WriteLine(assignedPort + " is assigned as new port");
+
+            serverPortReply.Close();
+            requestPortListener.Close();
+        }
+
+        private void SetText()
+        {
+            DataToSendTextBox.Text = assignedPort;
+        }
+    }
+}
---
+++ ServerSide.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+namespace TCPServerClientApplication
+{
+    public partial class ServerSide : Form
+    {
+        delegate void SetTextCallback(string text);
+
+        private string ipAddress = "";
+        private int port = 13000;
+
+        private int portForClient = 0;
+
+        public ServerSide()
+        {
+            InitializeComponent();
+        }
+        
+        private void PortButton_Click(object sender, EventArgs e)
+        {
+            ipAddress = textBox1.Text;
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+            informationBox.Text += "Destination IP: " + ipAddress + " Port: " + port + Environment.NewLine;
+            portForClient = GetAvailablePort();
+            try
+            {
+                s.Connect(ep);
+                s.Send(Encoding.ASCII.GetBytes(portForClient.ToString()), 0, portForClient.ToString().Length, SocketFlags.None);
+                s.Close();
+
+                Console.WriteLine("Server sent available port.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+        }
+
+        private int GetAvailablePort()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+
+            informationBox.Text += $"Current assigned port is: {port}{Environment.NewLine}";
+
+            return port;
+        }
+    }
+}
